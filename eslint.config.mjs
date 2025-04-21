6ca3b43c6adb26697229d04cdfc084c1d75ae814
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-21 15:05:06 +0100

    chore: add eslint and prettier setup

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  {
+    ...tseslint.configs.recommended,
+    rules: {
+      "react/react-in-jsx-escope": "off",
+    }
+  },
+  pluginReact.configs.flat.recommended,
+]);
