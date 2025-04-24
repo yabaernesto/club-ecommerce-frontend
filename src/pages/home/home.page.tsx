b2b0e2cb58dcd773f30cb821c22828207c0452fc
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-24 16:53:09 +0100

    chore: change Home Page div to fragment

---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,13 @@
+import Header from '../../components/header/header.component'
+import Categories from '../../components/categories/categories.component'
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Categories />
+    </>
+  )
+}
+
+export default HomePage
