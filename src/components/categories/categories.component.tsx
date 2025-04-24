f48a804e389e5d19d63d54aaef8975f2c99e9a14
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-24 20:23:58 +0100

    refactor: add Styled Component to Categories Component

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -7,7 +7,7 @@ import CategoryItem from '../category-item/category-item.component'
 import env from '../../config/env.config'
 
 // styles
-import './categories.styles.css'
+import { CategoriesContainer, CategoriesContent } from './categories.styles'
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([])
@@ -29,15 +29,15 @@ const Categories = () => {
   }, [])
 
   return (
-    <div className='categories-container'>
-      <div className='categories-content'>
+    <CategoriesContainer>
+      <CategoriesContent>
         {categories.map((category) => (
           <div key={category.id}>
             <CategoryItem category={category} />
           </div>
         ))}
-      </div>
-    </div>
+      </CategoriesContent>
+    </CategoriesContainer>
   )
 }
 
