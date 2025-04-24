2b615d858ade80c9c68d8f24da4a9017f51aa3d5
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-24 20:12:26 +0100

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,19 @@
+const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#F8F9FA',
+    dark: '#343A40'
+  },
+  background: {
+    dark: '#212529',
+    white: '#F8F9FA'
+  },
+  input: {
+    background: '#E9ECEF',
+    placeholder: '#6C757D'
+  },
+  error: '#FF6A6A',
+  success: '#198754'
+}
+
+export default Colors
