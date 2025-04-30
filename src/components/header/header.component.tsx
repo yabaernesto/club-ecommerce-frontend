6549eca0882b4771ecbc55063248490340312e80
Yaba Ernesto <yabaernesto@gmail.com>
2025-04-30 16:46:36 +0100

    feat: add home navigation to logo click

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -19,6 +19,10 @@ const Header = () => {
 
   const { isAuthenticated } = useContext(UserContext)
 
+  const handleLogoClick = () => {
+    navigate('/')
+  }
+
   const handleLoginClick = () => {
     navigate('/login')
   }
@@ -29,7 +33,7 @@ const Header = () => {
 
   return (
     <HeaderContainer>
-      <HeaderTitle>CLUB CLOTHING</HeaderTitle>
+      <HeaderTitle onClick={handleLogoClick}>CLUB CLOTHING</HeaderTitle>
       <HeaderItems>
         <HeaderItem>Explorar</HeaderItem>
         {!isAuthenticated && (
