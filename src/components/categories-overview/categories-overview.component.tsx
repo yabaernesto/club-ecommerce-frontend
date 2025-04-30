967016c4e099f0ba837dd4e2a84f152cb153a8b0
Yaba Ernesto <yabaernesto@gmail.com>
2025-05-01 00:32:19 +0100

    feat: add Loading

--- src/components/categories-overview/categories-overview.component.tsx
+++ src/components/categories-overview/categories-overview.component.tsx
@@ -2,18 +2,21 @@ import { useContext, useEffect } from 'react'
 
 import { CategoryContext } from '../../contexts/category.context'
 
+import CategoryOverview from '../category-overview/category-overview.component'
+import Loading from '../loading/loading.component'
+
 import { Container } from './categories-overview.styles'
 
-import CategoryOverview from '../category-overview/category-overview.component'
-
 const CategoriesOverview = () => {
-  const { categories, fetchCategories } = useContext(CategoryContext)
+  const { categories, isLoading, fetchCategories } = useContext(CategoryContext)
 
   useEffect(() => {
     if (categories.length === 0) {
       fetchCategories()
     }
   }, [])
+
+  if (isLoading) return <Loading />
 
   return (
     <Container>
