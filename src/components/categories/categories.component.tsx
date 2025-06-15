25faa90358010e616472f9391fad732437e4e21c
Yaba Ernesto <yabaernesto@gmail.com>
2025-06-16 00:05:44 +0100

    chore: aliases

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,11 +1,11 @@
-import { useContext, useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 
-import CategoryItem from '../category-item/category-item.component'
 import Loading from '../loading/loading.component'
-
-import { CategoriesContainer, CategoriesContent } from './categories.styles'
+import CategoryItem from '../category-item/category-item.component'
 
 import { CategoryContext } from '../../contexts/category.context'
+
+import { CategoriesContainer, CategoriesContent } from './categories.styles'
 
 const Categories = () => {
   const { fetchCategories, categories, isLoading } = useContext(CategoryContext)
